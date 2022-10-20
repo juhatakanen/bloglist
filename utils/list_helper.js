dcdabb03871f3e75f93b3c93baf7db626d1ad97d
Juha Takanen <juhatakanen@hotmail.com>
2022-10-20 12:23:39 +0300

    4.4 tehty

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
+
+
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+    const arrayOFLikes = blogs.map(blog => blog.likes)
+    const totalLikes =  arrayOFLikes.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+    return totalLikes
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
