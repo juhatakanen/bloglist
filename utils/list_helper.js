8ae2af96142d77767af39d745511c2d442e93518
Juha Takanen <juhatakanen@hotmail.com>
2022-10-20 12:49:22 +0300

    4.5* tehty

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,16 +1,26 @@
-
-
-const dummy = (blogs) => {
+const dummy = blogs => {
     return 1
-  }
+}
 
-const totalLikes = (blogs) => {
+const totalLikes = blogs => {
     const arrayOFLikes = blogs.map(blog => blog.likes)
     const totalLikes =  arrayOFLikes.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
     return totalLikes
 }
-  
-  module.exports = {
+
+const favouriteBlog = blogs => {
+    const arrayOFLikes = blogs.map(blog => blog.likes)
+    const biggestLikeValue = Math.max(...arrayOFLikes)
+    const blogsWithBiggestLikeValue = blogs.filter(blog => blog.likes === biggestLikeValue)
+    return {
+        "author": blogsWithBiggestLikeValue[0].author,
+        "likes": blogsWithBiggestLikeValue[0].likes,
+        "title": blogsWithBiggestLikeValue[0].title
+    } 
+}
+
+module.exports = {
     dummy,
-    totalLikes
-  }
+    totalLikes,
+    favouriteBlog
+}
